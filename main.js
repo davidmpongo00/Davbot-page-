e9613366c5e95a6c17a00a7370774730cff96b03
davidmpongo00 <scholampongo260@gmail.com>
2024-12-10 09:20:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const {
 const fs = require("fs");
 const SCRIPT_FILE = "page_bot.js";
 const SCRIPT_PATH = __dirname + "/" + SCRIPT_FILE;
-const GIT = process.env.repo || "https://github.com/davidmpongo00/Davbot-page-/";
+const GIT = process.env.repo || "https://github.com/neth7/Ws3-PageBot.git";
 
 async function Load() {
-  console.log(`Davbot page | Created by David mpongo`);
+  console.log(`Ws3 PageBot | Created by Kenneth Aceberos`);
   const execute = async (cmd) => {
     await new Promise(async (resolve, reject) => {
       const buang = await exec(cmd, {
