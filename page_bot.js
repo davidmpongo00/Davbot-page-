a71ff76632d5ce22fc1232fd78995c6552dc641a
davidmpongo00 <scholampongo260@gmail.com>
2024-12-10 09:04:12 +0100

    Update page_bot.js

--- page_bot.js
+++ page_bot.js
@@ -6,8 +6,8 @@ os = require('os'),
 fs = require('fs'),
 api = require('./ws3/api'),
 app = express(),
-VERIFY_TOKEN = 'ws3',
-PASSWORD_ADMIN = process.env.pass || 'ws3',
+VERIFY_TOKEN = 'Davbot',
+PASSWORD_ADMIN = process.env.pass || 'davbot',
 PAGE_ACCESS_TOKEN = api.PAGE_ACCESS_TOKEN,
 PORT = process.env.PORT || 3000
 
